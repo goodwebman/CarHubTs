9fc101560ed51a5685693b46082c03aecf0cd600
goodwebman <gitweb2danya@mail.ru>
2024-11-15 18:51:22 +0600

    finished footer

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -23,15 +23,17 @@ const Hero = () => {
 					handleClick={handleScroll}
 				/>
 			</div>
-      <div className='hero__image-container'>
-        <div className='hero__image'>
-            <Image src={'/hero.png'} alt='hero png' fill className='object-contain' />
-            <div className='hero__image-overlay'>
-
-            </div>
-
-        </div>
-      </div>
+			<div className='hero__image-container'>
+				<div className='hero__image'>
+					<Image
+						src={'/hero.png'}
+						alt='hero png'
+						fill
+						className='object-contain'
+					/>
+				</div>
+				<div className='hero__image-overlay'></div>
+			</div>
 		</div>
 	)
 }
