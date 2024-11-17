b3e1c24e0b4bf43fcc997670e604c268db2ce926
goodwebman <gitweb2danya@mail.ru>
2024-11-17 20:04:52 +0600

    use car API and start to create modal window

--- components/index.ts
+++ components/index.ts
@@ -2,10 +2,20 @@ import  Hero  from './Hero'
 import CustomButton from './CustomButton'
 import Footer from './Footer'
 import NavBar from './NavBar'
+import CustomFilter from './CustomFilter'
+import SearchBar from './SearchBar'
+import SearchManufacturer from './SearchManufacturer'
+import CarCard from './CarCard'
+import CarDetails from './CarDetails'
 
 export {
     Hero,
     CustomButton,
     Footer,
-    NavBar
+    NavBar,
+    CustomFilter,
+    SearchBar,
+    SearchManufacturer,
+    CarCard,
+    CarDetails
 }
